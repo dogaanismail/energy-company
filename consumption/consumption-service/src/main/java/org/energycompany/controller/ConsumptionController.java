7401eba5f4ff4ec199bf2853a924439357c9d420
dogaanismail <ismaildogaan@gmail.com>
2025-04-07 22:05:34 +0300

    - Introduced a sample JSON file for mock consumption response data.
    - Secured the `getConsumptions` endpoint with a PreAuthorize annotation, enforcing role-based access for users with the "CUSTOMER" authority.

--- consumption/consumption-service/src/main/java/org/energycompany/controller/ConsumptionController.java
+++ consumption/consumption-service/src/main/java/org/energycompany/controller/ConsumptionController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.energycompany.model.CustomResponse;
 import org.energycompany.model.consumption.ConsumptionResponse;
 import org.energycompany.service.ConsumptionService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,7 @@ public class ConsumptionController {
     private final ConsumptionService consumptionService;
 
     @GetMapping("/customer")
+    @PreAuthorize("hasAuthority('CUSTOMER')")
     public CustomResponse<List<ConsumptionResponse>> getConsumptions(
             @RequestParam(name = "customerId") UUID customerId,
             @RequestParam(name = "meteringPointId") UUID meteringPointId,
